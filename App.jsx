8f8ad357ef3e02d971f1c4669fd6f32e1f00ed75
Rubén García <rubengs.informatica@gmail.com>
2023-07-10 18:21:12 +0200

    category filter and menu working

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useMemo } from 'react';
 import 'react-native-gesture-handler';
 import { View, Text, Dimensions, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useRoute } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 import { initializeIptvApi } from './src/API/IPTV_API';
@@ -11,6 +11,18 @@ import Movies from './src/pages/Movies';
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
+
+function MoviesStack() {
+  const route = useRoute();
+  const category_id = route.params?.category_id;
+  
+  return (
+    <Stack.Navigator initialRouteName='Movies'>
+      <Stack.Screen name="Movies" component={Movies} options={{ headerShown: false }} initialParams={{ category_id: category_id }} />
+      <Stack.Screen name="SingleMovie" component={SingleMovie} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+}
 
 async function translateText(text) {
   const response = await fetch(
@@ -79,16 +91,15 @@ function App() {
       <NavigationContainer>
         <Drawer.Navigator initialRouteName="Movies" unmountInactiveScreens={true}>
           <Drawer.Screen
-            key= 'all-movies'
-            name= "TODAS LAS PELÍCULAS"
-            component={Movies}
-            initialParams='all-movies'
+            key='all-movies'
+            name="TODAS LAS PELÍCULAS"
+            component={MoviesStack}
           />
           {translatedCategories.map((category) => (
             <Drawer.Screen
               key={category.category_id}
               name={category.translatedCategoryName}
-              component={Movies}
+              component={MoviesStack}
               initialParams={{ category_id: category.category_id }}
             />
           ))}
